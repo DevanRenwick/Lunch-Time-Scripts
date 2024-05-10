3356f7a64221f09c0e73c3eb8c14a38535f45d6c
Devan Renwick <46578224+DevanRenwick@users.noreply.github.com>
2024-05-10 14:13:50 -0300

    Create SystemChanges.py
    Another script to work on.

---
+++ SystemChanges.py
@@ -0,0 +1,3 @@
+# This is a script that will monitor system changes and store logs on behalf of various things.
+# For starters, when files are deleted, added, modified.
+# I plan to grow this script as there are specific things I would like to monitor, example: CPU monitoring, RAM monitoring, Temp monitoring.

2d1f8a1930c94b62e6c4b1dd41dc0f30627e19c5
Devan Renwick <46578224+DevanRenwick@users.noreply.github.com>
2024-05-13 19:22:44 -0300

    Baby's first Batch file.

---
+++ Batch Scripts/Script.bat
@@ -0,0 +1,13 @@
+@echo off 
+::this will make it so the commands don't show when they're running.
+:: Comments also need to be on the next line after the command.
+echo hi
+::cls clears the screen
+::The go to command
+
+goto end
+::skips over the echo Devan command below.
+echo Devan
+
+:end
+pause
